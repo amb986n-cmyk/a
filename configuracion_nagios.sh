e3cb307ea266bfe752fc8720844d2df35ff7e4f5
amb986n-cmyk <amb986n@gmail.com>
2026-04-17 16:51:02 +0200

    Update configuracion_nagios.sh

--- configuracion_nagios.sh
+++ configuracion_nagios.sh
@@ -30,7 +30,8 @@ EOF
 ok 'Host añadido'; }
 fix_commands(){
  sed -i '/^define command{$/d' "$CMDS"
- sed -i '/^define command{$/N;/\n$/d' "$CMDS"
+ sed -i '/^define command{$/N;/
+$/d' "$CMDS"
 }
 prep_nrpe(){
  fix_commands; grep -q 'check_nrpe' "$CMDS" || cat >> "$CMDS" <<EOF
@@ -42,17 +43,35 @@ define command{
 EOF
 apt install -y nagios-nrpe-plugin; cp /usr/lib/nagios/plugins/check_nrpe /usr/local/nagios/libexec/; ok 'Servidor NRPE listo'; }
 ssh_client(){ read -p 'IP cliente: ' IP; read -p 'Usuario SSH: ' U; read -p 'IP servidor Nagios: ' SIP; ssh $U@$IP "sudo apt update && sudo apt install -y nagios-nrpe-server nagios-plugins wget && wget -O /tmp/check_mem.pl https://raw.githubusercontent.com/justintime/nagios-plugins/master/check_mem/check_mem.pl && sudo mv /tmp/check_mem.pl /usr/lib/nagios/plugins/check_mem && sudo chmod +x /usr/lib/nagios/plugins/check_mem && sudo sed -i 's/^allowed_hosts=.*/allowed_hosts=127.0.0.1,::1,$SIP/' /etc/nagios/nrpe.cfg && echo 'command[check_load]=/usr/lib/nagios/plugins/check_load -w 3,2.5,2 -c 6,5,4' | sudo tee -a /etc/nagios/nrpe.cfg && echo 'command[check_disk]=/usr/lib/nagios/plugins/check_disk -w 15% -c 5% -p /' | sudo tee -a /etc/nagios/nrpe.cfg && echo 'command[check_mem]=/usr/lib/nagios/plugins/check_mem -u -w 80 -c 90' | sudo tee -a /etc/nagios/nrpe.cfg && sudo systemctl restart nagios-nrpe-server"; ok 'Cliente configurado'; }
-add_checks(){ read -p 'Host existente: ' H; while true; do echo '1 CPU | 2 RAM | 3 Disco | 4 Ping | 5 Salir'; read -p 'Opción: ' c; case $c in 1)D='CPU Load';C='check_nrpe!check_load';;2)D='RAM';C='check_nrpe!check_mem';;3)D='Disco';C='check_nrpe!check_disk';;4)D='PING';C='check_ping!100.0,20%!500.0,60%';;5)break;;*)continue;;esac; cat >> "$HOSTS" <<EOF
+add_checks(){
+ read -p 'Host existente: ' H
+ grep -q "host_name $H" "$HOSTS" || { warn 'Host no existe'; return; }
+ while true; do
+  echo '1 CPU | 2 RAM | 3 Disco | 4 Ping | 5 Salir'
+  read -p 'Opción: ' c
+  case $c in
+   1)D='CPU_Load';C='check_nrpe!check_load';;
+   2)D='RAM';C='check_nrpe!check_mem';;
+   3)D='Disco';C='check_nrpe!check_disk';;
+   4)D='PING';C='check_ping!100.0,20%!500.0,60%';;
+   5)break;;
+   *)continue;;
+  esac
+  grep -q "service_description $D" "$HOSTS" && { warn 'Check ya existe'; continue; }
+  if grep -q 'contact_name admin' "$CONTACTS"; then EXTRA=' contacts admin'; else EXTRA=''; fi
+  cat >> "$HOSTS" <<EOF
 
 define service {
  use generic-service
  host_name $H
  service_description $D
  check_command $C
- contacts admin
+$EXTRA
 }
 EOF
-ok "$D añadido"; done }
+  ok "$D añadido"
+ done
+}
 mail_alert(){ apt install -y mailutils; read -p 'Correo: ' M; grep -q 'contact_name admin' "$CONTACTS" || cat >> "$CONTACTS" <<EOF
 
 define contact{
